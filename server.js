ff4fd3c06d76852478fa80cac4b962f34556908b
Oded Sagir <odedsagir@gmail.com>
2014-09-17 17:17:52 +0300

    server.js refactor

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var express = require('express'),
 app.use(express.static(__dirname + '/' + staticDir));
 
 app.get('*', function (req, res) {
-	res.sendfile(__dirname + '/' + staticDir + '/index.html');
+	res.sendFile(__dirname + '/' + staticDir + '/index.html');
 });
 
 var server = app.listen(process.env.PORT || 8088, function() {
