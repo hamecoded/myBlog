6cc32a0054691a2b8f65ff3b16261c8a3b8f49ef
Oded Sagir <odedsagir@gmail.com>
2014-09-18 12:14:09 +0300

    Make rjs truncation work; both dev and dist configs are now working
    make rjs truncation work; both dev and dist configs are now working

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,11 +132,67 @@ module.exports = function (grunt) {
         requirejs: {
           compile: {
             options: {
-                name: 'js/app',
                 baseUrl: 'public',
-                //mainConfigFile: 'public/js/require.config.js',
-                //include:  ['js/test.js']
-                out: 'tmp/<%= pkg.name %>.js'
+                name: 'js/app',
+                findNestedDependencies: true,  //truncates to output file all dependecies that are parsed so that you don't need to include them seperatly
+                out: 'tmp/<%= pkg.name %>.js',
+                //mainConfigFile: 'public/js/require.config.js',  //BUG: doesn't get applied
+                paths: {
+                        // Libraries
+                        // ---------
+                        "vendor": "./bower_components",
+                        "underscore": "./bower_components/underscore/underscore",
+                        "mustache": "./bower_components/mustache/mustache",
+                        "jquery": "./bower_components/jquery/dist/jquery.min",
+                        "backbone": "./bower_components/backbone/backbone",
+                        "backbone.wreqr": "./bower_components/backbone.wreqr/lib/backbone.wreqr.min",
+                        "backbone.babysitter": "./bower_components/backbone.babysitter/lib/backbone.babysitter.min",
+                        "marionette": "./bower_components/marionette/lib/backbone.marionette.min",
+                        "bootstrap": "./bower_components/bootstrap/dist/js/bootstrap.min",
+
+                        // Require Modules to be used as pragmas
+                        // ---------
+                        "text": "./bower_components/requirejs-text/text",
+
+                        // Backbone Folder Structure
+                        // -------------------
+                        "models": "js/models",
+                        "collections": "collections",
+                        "routers": "routers",
+                        "views": "views",
+                        "templates": "./templates"
+                },
+                shim:{
+                    //Globally exposed variables:
+                    // $ jQuery Backbone Marionette _ 
+                    //unexposed globally variables:
+                    // mustache(need exposing inorder to be used) 
+                    // bootstrap(built ontop of $)
+                    "backbone":{
+                        "deps":[
+                            "underscore",
+                            "mustache"
+                        ]
+                    },
+                    "backbone.wreqr":{
+                        "deps": ["backbone"]
+                    },
+                    "backbone.babysitter":{
+                        "deps": ["backbone"]
+                    },
+                    "marionette":{
+                        "exports": "Marionette",
+                        "deps":[
+                            "backbone",
+                            "backbone.wreqr",
+                            "backbone.babysitter"
+                        ]
+                    },
+                    "bootstrap":{
+                        "deps":["jquery"]
+                    }
+                }
+                
             }
           }
         },
--- public/js/require.config.js
+++ public/js/require.config.js
@@ -2,38 +2,36 @@ define({
     paths:{
         // Libraries
         // ---------
-        "vendor": "../bower_components",
-        "underscore": "../bower_components/underscore/underscore",
-        "mustache": "../bower_components/mustache/mustache",
-        "jquery": "../bower_components/jquery/dist/jquery.min",
-        "backbone": "../bower_components/backbone/backbone.min",
-        "backbone.wreqr": "../bower_components/backbone.wreqr/lib/backbone.wreqr.min",
-        "backbone.babysitter": "../bower_components/backbone.babysitter/lib/backbone.babysitter.min",
-        "marionette": "../bower_components/marionette/lib/backbone.marionette.min",
-        "bootstrap": "../bower_components/bootstrap/dist/js/bootstrap.min",
+        "vendor": "bower_components",
+        "underscore": "bower_components/underscore/underscore",
+        "mustache": "bower_components/mustache/mustache",
+        "jquery": "bower_components/jquery/dist/jquery.min",
+        "backbone": "bower_components/backbone/backbone", //fully amd
+
+        "marionette": "bower_components/marionette/lib/backbone.marionette.min", //already includes wreqr and babysitter
+        "backbone.wreqr": "bower_components/backbone.wreqr/lib/backbone.wreqr.min", //kept for the backbone only example
+        "backbone.babysitter": "bower_components/backbone.babysitter/lib/backbone.babysitter.min",
+
+        "bootstrap": "bower_components/bootstrap/dist/js/bootstrap.min", //non-amd
 
         // Require Modules to be used as pragmas
         // ---------
-        "text": "../bower_components/requirejs-text/text",
+        "text": "bower_components/requirejs-text/text",
 
         // Backbone Folder Structure
         // -------------------
-        "models": "models",
+        "models": "js/models",
         "collections": "collections",
         "routers": "routers",
         "views": "views",
-        "templates": "../templates"
+        "templates": "templates"
     },
     shim:{
-        "underscore":{
-            // Exports the global window._ object
-            "exports": "_"
-        },
-        "jquery":{
-            "exports": "$"
-        },
+        //Globally exposed variables:
+        //$ jQuery Backbone _ 
+        //unexposed globally variables:
+        //mustache(need exposing inorder to be used) bootstrap(built ontop of $)
         "backbone":{
-            "exports": "Backbone",
             "deps":[
                 "underscore",
                 "mustache"
@@ -46,7 +44,6 @@ define({
             "deps": ["backbone"]
         },
         "marionette":{
-            "exports": "Marionette",
             "deps":[
                 "backbone",
                 "backbone.wreqr",
